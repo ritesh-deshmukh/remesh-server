28de7e0f7c5f71dc32981f7a78ba929fd412faaf
ritesh-deshmukh <riteshdeshmukh260@gmail.com>
2018-09-04 15:52:25 -0500

    q & a

--- script2.py
+++ script2.py
@@ -4,11 +4,12 @@ from compute.users import age,sex,income,livingEnvironment
 users = requests.get('http://localhost:8080/users').json()
 votes = requests.get('http://localhost:8080/votes').json()
 messages = requests.get('http://localhost:8080/messages').json()
+questions = requests.get('http://localhost:8080/questions').json()
 # print(len(messages))
-
 
 filtered_users = []
 filtered_votes = []
+filtered_messages = []
 
 for i in range(len(users)):
     if \
@@ -28,7 +29,12 @@ for i in range(len(votes)):
 
 for i in range(len(messages)):
     if messages[i]['id'] in filtered_votes:
-        print(messages[i]['text'])
+        filtered_messages.append(messages[i]['questionId'])
+        # print(f"Message(answer) text: {messages[i]['text']}")
+
+for i in range(len(questions)):
+    if questions[i]['id'] in filtered_messages:
+        print(f"Question text: {questions[i]['text']}")
 # print(count)
 # print(filtered_votes)
 # print(answer)
